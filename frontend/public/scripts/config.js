7782ce945d77af8086d386ac597d0ebba3c1cf9d
Thara Waranuset <tharawaranuset@gmail.com>
2024-11-15 17:56:19 +0700

    some change for index

--- frontend/public/scripts/config.js
+++ frontend/public/scripts/config.js
@@ -1,5 +1,5 @@
 // TODO0: change the BACKEND_URL to your EC2 public IPv4
-export const BACKEND_URL = "http://54.172.227.199:3222";
+export const BACKEND_URL = "http://localhost:3222";
 
 // TODO0: change element in MEMBERS to your group members.
 // WARNING: the _id parameters is required.
