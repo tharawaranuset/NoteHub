04c2a01295fee8e24a692c6872a27542b490f1da
Thara Waranuset <tharawaranuset@gmail.com>
2024-11-23 23:14:11 +0700

    fix some bug

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import express from "express";
 import cors from "cors";
 
@@ -20,28 +20,3 @@ app.use("/items", ItemRoute);
 app.use("/members", MemberRoute);
 app.use("/comments", commentRoutes);
 export default app;
-||||||| empty tree
-=======
-import express from "express";
-import cors from "cors";
-
-import ItemRoute from "./routes/itemRoute.js";
-import MemberRoute from "./routes/memberRoute.js";
-import LikeRoute from "./routes/likeRoute.js";
-import commentRoutes from "./routes/commentRoute.js";
-const app = express();
-
-// body-parser
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-// allow request from other origin (Frontend which is at different port)
-app.use(cors());
-
-// use routes
-app.use("/items", ItemRoute);
-app.use("/members", MemberRoute);
-app.use("/likes", LikeRoute);
-app.use("/comments", commentRoutes);
-export default app;
->>>>>>> 4cbffb31080ae9ff1a84befa383667974a90a44d
